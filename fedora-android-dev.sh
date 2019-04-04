e2a493a9ba20c6849d3e8671da6b1b3af24fb879
Anik1199 <anik@lineageos.org>
2019-04-04 19:51:49 +0600

    Python 2.7 is required for repo

--- fedora-android-dev.sh
+++ fedora-android-dev.sh
@@ -3,11 +3,11 @@
 clear
 
 echo -e "Setting up android development tools"
-sudo dnf install autoconf213 bison bzip2 ccache curl flex gawk gcc-c++ git glibc-devel \
-    glibc-static libstdc++-static libX11-devel make mesa-libGL-devel ncurses-devel patch zlib-devel \
+sudo dnf install  android-tools autoconf213 bison bzip2 ccache curl flex gawk gcc-c++ git glibc-devel \
+    glibc-static libstdc++-static libX11-devel make mesa-libGL-devel ncurses-devel patch python27 zlib-devel \
     ncurses-devel.i686 readline-devel.i686 zlib-devel.i686 libX11-devel.i686 mesa-libGL-devel.i686 \
     glibc-devel.i686 libstdc++.i686 libXrandr.i686 zip perl-Digest-SHA wget lzop openssl-devel \
-    java-1.8.0-openjdk-devel ImageMagick ncurses-compat-libs schedtool android-tools
+    java-1.8.0-openjdk-devel ImageMagick ncurses-compat-libs schedtool
 
 sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libncurses.so.5
 sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libtinfo.so.5
